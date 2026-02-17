bec5780a5b4e11405bed7df58d8e5401965f182e
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-17 10:06:54 +0900

    Update physics.js

--- js/physics.js
+++ js/physics.js
@@ -5,8 +5,18 @@ export function updateRotation(rotation, velocity) {
 }
 
 export function updateBall(angle, velocity){
-  angle -= velocity; // 逆方向
+  angle -= velocity;
   velocity *= 0.992;
   return { angle, velocity };
 }
 
+export function updateDrop(radiusRatio){
+  // 半径をゆっくり縮める
+  radiusRatio -= 0.01;
+
+  if(radiusRatio < 0.55){
+    radiusRatio = 0.55; // 内側限界
+  }
+
+  return radiusRatio;
+}
