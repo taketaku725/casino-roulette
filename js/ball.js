060fbfd06d9a5f7a6b3067baa7a0fee6f9543711
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-17 10:00:46 +0900

    Update ball.js

--- js/ball.js
+++ js/ball.js
@@ -1,1 +1,22 @@
+export function drawBall(ctx, canvas, angle, radiusRatio){
+  const center = canvas.width / 2;
+  const outerRadius = center * 0.95; // 外周
+  const ballRadius = center * 0.03;
 
+  const r = outerRadius * radiusRatio;
+
+  const x = center + Math.cos(angle) * r;
+  const y = center + Math.sin(angle) * r;
+
+  // 影
+  ctx.beginPath();
+  ctx.arc(x+2, y+2, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "rgba(0,0,0,0.3)";
+  ctx.fill();
+
+  // 球
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
