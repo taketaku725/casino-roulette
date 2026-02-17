ca746a0341756f96d70504c34add7b586dffe5e7
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-17 10:53:57 +0900

    Update ball.js

--- js/ball.js
+++ js/ball.js
@@ -1,22 +1,36 @@
-export function drawBall(ctx, canvas, angle, radiusRatio){
+export function drawBall(ctx, canvas, angle, radiusRatio, height){
+
   const center = canvas.width / 2;
-  const outerRadius = center * 0.95; // 外周
-  const ballRadius = center * 0.03;
+  const outerRadius = center * 0.95;
+  const baseRadius = center * 0.03;
 
   const r = outerRadius * radiusRatio;
-
   const x = center + Math.cos(angle) * r;
   const y = center + Math.sin(angle) * r;
 
-  // 影
+  const ballRadius = baseRadius * (1 + height*0.3);
+
+  // 影（高さで拡散）
   ctx.beginPath();
-  ctx.arc(x+2, y+2, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "rgba(0,0,0,0.3)";
+  ctx.arc(x, y+height*10, ballRadius*1.2, 0, Math.PI*2);
+  ctx.fillStyle = `rgba(0,0,0,${0.3 - height*0.2})`;
   ctx.fill();
 
   // 球
+  const grad = ctx.createRadialGradient(
+    x-ballRadius*0.3,
+    y-ballRadius*0.3,
+    ballRadius*0.1,
+    x,
+    y,
+    ballRadius
+  );
+
+  grad.addColorStop(0,"#fff");
+  grad.addColorStop(1,"#ccc");
+
   ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "white";
+  ctx.arc(x, y - height*5, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = grad;
   ctx.fill();
 }
