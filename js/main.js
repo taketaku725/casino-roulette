6dffd9ab2cfa9942f1f32fd31240f1664857ff68
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-17 10:08:07 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { updateRotation } from "./physics.js";
 import { drawWheel } from "./wheel.js";
 import { updateBall } from "./physics.js";
 import { drawBall } from "./ball.js";
+import { updateDrop } from "./physics.js";
 
 const canvas = document.getElementById("roulette");
 const ctx = canvas.getContext("2d");
@@ -29,11 +30,13 @@ spinBtn.onclick = () => {
   velocity = 0.3 + Math.random()*0.05;      // ホイール
   ballVelocity = 0.6 + Math.random()*0.1;  // 球は速く
   ballAngle = 0;
+  ballRadiusRatio = 1;
 
   currentState = STATE.SPINNING;
 };
 
 function loop(){
+
   if(currentState === STATE.SPINNING){
 
     const wheelResult = updateRotation(rotation, velocity);
@@ -44,18 +47,34 @@ function loop(){
     ballAngle = ballResult.angle;
     ballVelocity = ballResult.velocity;
 
-    if(velocity < 0.002 && ballVelocity < 0.01){
-      velocity = 0;
-      ballVelocity = 0;
+    // 球が一定以下で落下開始
+    if(ballVelocity < 0.15){
+      currentState = STATE.DROPPING;
+    }
+  }
+
+  if(currentState === STATE.DROPPING){
+
+    const ballResult = updateBall(ballAngle, ballVelocity);
+    ballAngle = ballResult.angle;
+    ballVelocity = ballResult.velocity;
+
+    radiusRatio = updateDrop(radiusRatio);
+
+    // 落ちきったら停止準備
+    if(radiusRatio <= 0.55 && ballVelocity < 0.02){
       currentState = STATE.IDLE;
+      ballVelocity = 0;
+      velocity = 0;
     }
   }
 
   drawWheel(ctx, canvas, rotation);
-  drawBall(ctx, canvas, ballAngle, ballRadiusRatio);
+  drawBall(ctx, canvas, ballAngle, radiusRatio);
 
   requestAnimationFrame(loop);
 }
 
 loop();
+
 
