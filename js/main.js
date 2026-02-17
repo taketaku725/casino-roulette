0629fb1422ef643ce65b9d55d00886575a3b43c6
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-17 10:02:46 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { STATE } from "./state.js";
 import { updateRotation } from "./physics.js";
 import { drawWheel } from "./wheel.js";
+import { updateBall } from "./physics.js";
+import { drawBall } from "./ball.js";
 
 const canvas = document.getElementById("roulette");
 const ctx = canvas.getContext("2d");
@@ -9,6 +11,9 @@ const spinBtn = document.getElementById("spinBtn");
 let rotation = 0;
 let velocity = 0;
 let currentState = STATE.IDLE;
+let ballAngle = 0;
+let ballVelocity = 0;
+let ballRadiusRatio = 1;
 
 function resize(){
   const size = Math.min(window.innerWidth, window.innerHeight) * 0.9;
@@ -20,24 +25,37 @@ window.addEventListener("resize", resize);
 
 spinBtn.onclick = () => {
   if(currentState !== STATE.IDLE) return;
-  velocity = 0.3 + Math.random()*0.05;
+
+  velocity = 0.3 + Math.random()*0.05;      // ホイール
+  ballVelocity = 0.6 + Math.random()*0.1;  // 球は速く
+  ballAngle = 0;
+
   currentState = STATE.SPINNING;
 };
 
 function loop(){
   if(currentState === STATE.SPINNING){
-    const result = updateRotation(rotation, velocity);
-    rotation = result.rotation;
-    velocity = result.velocity;
 
-    if(velocity < 0.002){
+    const wheelResult = updateRotation(rotation, velocity);
+    rotation = wheelResult.rotation;
+    velocity = wheelResult.velocity;
+
+    const ballResult = updateBall(ballAngle, ballVelocity);
+    ballAngle = ballResult.angle;
+    ballVelocity = ballResult.velocity;
+
+    if(velocity < 0.002 && ballVelocity < 0.01){
       velocity = 0;
+      ballVelocity = 0;
       currentState = STATE.IDLE;
     }
   }
 
   drawWheel(ctx, canvas, rotation);
+  drawBall(ctx, canvas, ballAngle, ballRadiusRatio);
+
   requestAnimationFrame(loop);
 }
 
 loop();
+
