b5c4edd949838d2e5166d1f27c0e617eb4661e03
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-17 10:01:17 +0900

    Update physics.js

--- js/physics.js
+++ js/physics.js
@@ -3,3 +3,10 @@ export function updateRotation(rotation, velocity) {
   velocity *= 0.995;
   return { rotation, velocity };
 }
+
+export function updateBall(angle, velocity){
+  angle -= velocity; // 逆方向
+  velocity *= 0.992;
+  return { angle, velocity };
+}
+
