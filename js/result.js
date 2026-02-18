f9dbd05c3209e9faad6056cc6ac942e654533ec7
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-18 17:14:34 +0900

    Update result.js

--- js/result.js
+++ js/result.js
@@ -3,7 +3,7 @@ export function getWinningNumber(ballAngle, rotation, numbers){
   const total = numbers.length;
   const slice = (Math.PI * 2) / total;
 
-  const adjusted = (ballAngle + rotation) % (Math.PI*2);
+  const adjusted = (ballAngle + rotation + Math.PI/2) % (Math.PI*2);
   const modAngle = adjusted < 0 ? adjusted + Math.PI*2 : adjusted;
 
   const index = Math.floor(modAngle / slice);
